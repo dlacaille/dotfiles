47dbc4e25cba1577ed14cdaeb805c63824b19fbf
Dominic Lacaille <dominic@swoo.ca>
2022-04-15 23:15:11 -0400

    Add mappings and plugins

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,11 +1,16 @@
 -- General
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.transparent_window = true
-vim.opt.showtabline = 2 -- Always show tab line
+lvim.colorscheme = "onedarkpro"
+vim.opt.background = "dark"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 4 -- The number of spaces inserted for each indentation
 vim.opt.tabstop = 4 -- Insert 2 spaces for a tab
+vim.opt.titlestring = "%{split(getcwd(), '/')[-1]} - %t"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 20
+vim.cmd("set diffopt+=hiddenoff,iwhiteall,algorithm:patience")
 
 -- Hide ^M on mixed unix/dos line ending files
 vim.cmd "match Ignore /\r$/"
@@ -16,11 +21,18 @@ lvim.leader = "space"
 -- Custom mappings
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
+-- Quick wins
+lvim.keys.normal_mode[",,"] = "A,<esc>"
+lvim.keys.normal_mode[",o"] = "A,<esc>o"
+lvim.keys.normal_mode[";;"] = "A;<esc>"
+lvim.keys.normal_mode[";o"] = "A;<esc>o"
+
 -- Homerow navigation
-lvim.keys.normal_mode["<C-h>"] = "^"
-lvim.keys.normal_mode["<C-l>"] = "$"
-lvim.keys.normal_mode["<C-j>"] = "jjjj"
-lvim.keys.normal_mode["<C-k>"] = "kkkk"
+-- Commented to force myself to use lightspeed instead
+-- lvim.keys.normal_mode["<C-h>"] = "^"
+-- lvim.keys.normal_mode["<C-l>"] = "$"
+-- lvim.keys.normal_mode["<C-j>"] = "jjjj"
+-- lvim.keys.normal_mode["<C-k>"] = "kkkk"
 
 -- Natural editing in insert mode
 lvim.keys.insert_mode["<M-bs>"] = {"<cmd>:norm db<cr>", {silent = true}}
@@ -28,23 +40,14 @@ lvim.keys.insert_mode["<M-del>"] = {"<cmd>:norm dw<cr>", {silent = true}}
 lvim.keys.insert_mode["<M-left>"] = {"<cmd>:norm bi<cr>", {silent = true}}
 lvim.keys.insert_mode["<M-right>"] = {"<cmd>:norm w<cr>", {silent = true}}
 
--- Autocommands
-lvim.autocommands.custom_groups = {
-    -- On entering insert mode in any file, change fileformat
-    {"InsertEnter", "*", ":set ff=unix"}
-}
+-- Terminal config
+lvim.builtin.terminal.direction = "horizontal"
+lvim.builtin.terminal.size = 15
+lvim.builtin.terminal.shade_terminals = false
 
--- NPM scripts
-lvim.builtin.which_key.mappings["n"] = {
-    name = "NPM",
-    i = {
-        "<cmd>lua require('lvim.core.terminal')._exec_toggle({cmd = 'npm install', count = 201, direction = 'float'})<cr>",
-        "Install packages"
-    },
-    rw = {
-        "<cmd>lua require('lvim.core.terminal')._exec_toggle({cmd = 'npm run watch', count = 202, direction = 'float'})<cr>",
-        "Run watch"
-    }
+-- Git mappings
+lvim.builtin.which_key.mappings["g"]["h"] = {
+    "<cmd>:DiffviewFileHistory<cr>", "File History"
 }
 
 -- Activate some default plugins
@@ -56,19 +59,14 @@ lvim.builtin.terminal.active = true
 -- Configure greeter
 lvim.builtin.alpha.dashboard.section.header.val = {
     [[                                                                                ]],
-    [[                                                                                ]],
-    [[                                                                                ]],
-    [[                                                                ⣤⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣦⣄  ]],
-    [[ ⢀⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡶                              ⣀⣠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣄⣀   ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆ ]],
-    [[ ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇                             ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄  ⢸⣿⣿⣿       ⢸⣿⣿⣿⡇ ]],
-    [[ ⢸⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀    ⣀⣀⣀⡀    ⢀⣀⣀⣀     ⣀⣀⣀⡀    ⢸⣿⣿⣿⠉⠉⠉⠉⠉⠉⠉⠉⣿⣿⣿⡇  ⢸⣿⣿⣿       ⢸⣿⣿⣿⡇ ]],
-    [[ ⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    ⣿⣿⣿⡇    ⢸⣿⣿⣿     ⣿⣿⣿⡇    ⢸⣿⣿⣿        ⣿⣿⣿⡇  ⢸⣿⣿⣿       ⢸⣿⣿⣿⡇ ]],
-    [[ ⢀⣀⣉⣉⣉⣉⣉⣉⣉⣉⣉⣹⣿⣿⣿    ⣿⣿ Agence Numérique - https://swoo.ca ⣿⣿⡇  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇ ]],
-    [[ ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿    ⣿⣿⣿⡇    ⢸⣿⣿⣿     ⣿⣿⣿⡇    ⢸⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⡇   ⠛⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋  ]],
-    [[ ⠘⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠁    ⣿⣿⣿⡇    ⢸⣿⣿⣿     ⣿⣿⣿⡇    ⠈⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟                    ]],
-    [[                    ⣿⣿⣿⣧⣤⣤⣤⣤⣼⣿⣿⣿⣤⣤⣤⣤⣤⣿⣿⣿⡇                                       ]],
-    [[                    ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠇                                       ]],
-    [[                                                                                ]],
+    [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+    [[⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⠀⠀]],
+    [[⠀⢸⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠁⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⡿⠿⠿⠿⠿⠿⠿⠿⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⠀⠀]],
+    [[⠀⢸⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣄⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀]],
+    [[⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿ Agence Numérique - https://swoo.ca ⣿⡇⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀]],
+    [[⠀⢠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣼⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣤⣤⣤⣤⣼⣿⣿⣿⣤⣤⣤⣤⣤⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤⣤⣿⣿⣿⡇⠀⠀⠀⢸⣿⣿⣿⣤⣤⣤⣤⣤⣤⣤⣼⣿⣿⣿⠀⠀]],
+    [[⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀]],
+    [[⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀]],
     [[                                                                                ]]
 }
 
@@ -78,13 +76,15 @@ lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.setup.disable_netrw = 0
 lvim.builtin.nvimtree.setup.hijack_netrw = 0
 
--- Taste the rainbow
-require("nvim-treesitter.configs").setup {
-    rainbow = {
-        enable = true,
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil -- Do not enable for files with more than n lines, int
-    }
+-- Add ignores to telescope
+lvim.builtin.telescope.defaults.file_ignore_patterns = {
+    "vendor/*", "dist/*", "%.lock", "__pycache__/*", "%.sqlite3", "%.ipynb",
+    "node_modules/*", "%.jpg", "%.jpeg", "%.png", "%.svg", "%.otf", "%.ttf",
+    ".git/", "%.webp", ".dart_tool/", ".github/", ".gradle/", ".idea/",
+    ".settings/", ".vscode/", "__pycache__/", "build/", "env/", "gradle/",
+    "node_modules/", "target/", "%.pdb", "%.dll", "%.class", "%.exe", "%.cache",
+    "%.ico", "%.pdf", "%.dylib", "%.jar", "%.docx", "%.met", "smalljre_*/*",
+    ".vale/", "package-lock.json", "%.map", "public/*"
 }
 
 -- if you don't want all the parsers change this to a table of the ones you want
@@ -106,8 +106,7 @@ formatters.setup {
     {
         exe = "phpcsfixer",
         args = {"--config", "/Users/swoo/.swoo-workspace/.php-cs-fixer.php"}
-    }, {exe = "prettier", args = {"--tab-width", "4", "--single-quote"}},
-    {exe = "lua-format"}
+    }, {exe = "prettierd"}, {exe = "lua-format"}
 }
 
 -- Set additional linters
@@ -115,65 +114,170 @@ local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {{exe = "eslint_d"}}
 
 -- Additional Plugins
-lvim.plugins = {
-    {"cakebaker/scss-syntax.vim"}, {
-        "olimorris/onedarkpro.nvim",
-        config = function()
-            local onedarkpro = require("onedarkpro")
-            onedarkpro.setup({
-                colors = {bg = "#131419"}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
-                options = {
-                    undercurl = true, -- Use the themes opinionated undercurl styles?
-                    cursorline = true, -- Use cursorline highlighting?
-                    transparency = true, -- Use a transparent background?
-                    terminal_colors = true -- Use the theme's colors for Neovim's :terminal?
-                },
-                styles = {comments = "italic", keywords = "italic"}
-            })
-            onedarkpro.load()
-            lvim.colorscheme = "onedarkpro"
-        end
-    }, {"tpope/vim-surround"}, {"p00f/nvim-ts-rainbow"},
-    {"mg979/vim-visual-multi"}, {"jeffkreeftmeijer/vim-numbertoggle"}, {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({"*"}, {
-                RGB = true, -- #RGB hex codes
-                RRGGBB = true, -- #RRGGBB hex codes
-                RRGGBBAA = true, -- #RRGGBBAA hex codes
-                rgb_fn = true, -- CSS rgb() and rgba() functions
-                hsl_fn = true, -- CSS hsl() and hsla() functions
-                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn,
-                mode = "background"
-            })
-        end
-    }, {
-        "gelguy/wilder.nvim",
-        config = function()
-            local wilder = require('wilder')
-            wilder.setup({modes = {':', '/', '?'}})
-            wilder.set_option('renderer', wilder.popupmenu_renderer({
-                highlighter = wilder.basic_highlighter(),
-                highlights = {
-                    accent = wilder.make_hl('WilderAccent', 'Pmenu', {
-                        {a = 1}, {a = 1}, {foreground = '#ca62f8'}
-                    })
-                },
-                left = {' ', wilder.popupmenu_devicons()},
-                right = {' ', wilder.popupmenu_scrollbar()}
-            }))
+lvim.plugins = {}
+function use(plugin) table.insert(lvim.plugins, plugin) end
 
-        end
-    }, {"felipec/vim-sanegx"}, {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("indent_blankline").setup {show_current_context = true}
-        end
-    }, {"captbaritone/better-indent-support-for-php-with-html", ff = "php"}
+-- Colorschemes
+use {"lunarvim/colorschemes"}
+use {"folke/tokyonight.nvim"}
+use {"olimorris/onedarkpro.nvim"}
+use {"luxed/ayu-vim"}
+
+-- Configure theme
+local onedarkpro = require("onedarkpro")
+onedarkpro.setup({
+    colors = {bg = "#131419"}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
+    options = {
+        undercurl = true, -- Use the themes opinionated undercurl styles?
+        cursorline = true -- Use cursorline highlighting?
+    },
+    styles = {comments = "italic", keywords = "italic"}
+})
+onedarkpro.load()
+
+-- Fixes the syntax for scss files
+use {"cakebaker/scss-syntax.vim"}
+
+-- Necessities
+use {"tpope/vim-surround"}
+use {"jeffkreeftmeijer/vim-numbertoggle"}
+
+-- 🐙 Octo
+use {"pwntester/octo.nvim", config = function() require"octo".setup() end}
+
+-- Shows diff view in a tab
+use {"sindrets/diffview.nvim"}
+
+-- Highlight colors
+use {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+        require("colorizer").setup({
+            "css", "scss", "javascript", "typescript", "vue", "php", "html",
+            "lua", "vim"
+        }, {
+            RGB = true, -- #RGB hex codes
+            RRGGBB = true, -- #RRGGBB hex codes
+            RRGGBBAA = true, -- #RRGGBBAA hex codes
+            rgb_fn = true, -- CSS rgb() and rgba() functions
+            hsl_fn = true, -- CSS hsl() and hsla() functions
+            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn,
+            mode = "background"
+        })
+    end
 }
 
--- Configure indent-blankline
-vim.g.indent_blankline_filetype_exclude = {
-    'alpha', 'terminal', 'lspinfo', 'checkhealth', 'help'
+-- Better quickfix window
+use {
+    "kevinhwang91/nvim-bqf",
+    event = {"BufRead", "BufNew"},
+    config = function()
+        require("bqf").setup({
+            auto_enable = true,
+            preview = {
+                win_height = 12,
+                win_vheight = 12,
+                delay_syntax = 80,
+                border_chars = {
+                    "┃", "┃", "━", "━", "┏", "┓", "┗", "┛",
+                    "█"
+                }
+            },
+            func_map = {vsplit = "", ptogglemode = "z,", stoggleup = ""},
+            filter = {
+                fzf = {
+                    action_for = {["ctrl-s"] = "split"},
+                    extra_opts = {
+                        "--bind", "ctrl-o:toggle-all", "--prompt", "> "
+                    }
+                }
+            }
+        })
+    end
 }
+
+-- Let's fly
+use {"ggandor/lightspeed.nvim", event = "BufRead"}
+
+-- Smooth sailing
+use {
+    "karb94/neoscroll.nvim",
+    event = "BufRead",
+    config = function()
+        require('neoscroll').setup()
+
+        local t = {}
+        -- Syntax: t[keys] = {function, {function arguments}}
+        t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '150'}}
+        t['<C-d>'] = {'scroll', {'vim.wo.scroll', 'true', '150'}}
+        t['<C-b>'] = {
+            'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '150'}
+        }
+        t['<C-f>'] = {
+            'scroll', {'vim.api.nvim_win_get_height(0)', 'true', '150'}
+        }
+        t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
+        t['<C-e>'] = {'scroll', {'0.10', 'false', '100'}}
+        t['zt'] = {'zt', {'150'}}
+        t['zz'] = {'zz', {'150'}}
+        t['zb'] = {'zb', {'150'}}
+
+        require('neoscroll.config').set_mappings(t)
+    end
+}
+
+-- HTML Tag magic
+use {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    config = function() require("nvim-ts-autotag").setup() end
+}
+
+-- Taste the rainbow
+use {
+    "p00f/nvim-ts-rainbow",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            rainbow = {
+                enable = true,
+                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                max_file_lines = nil -- Do not enable for files with more than n lines, int
+            }
+        }
+    end
+}
+
+-- Better command palette
+use {
+    "gelguy/wilder.nvim",
+    config = function()
+        local wilder = require('wilder')
+        wilder.setup({modes = {':', '/', '?'}})
+        local opts = {
+            highlighter = wilder.basic_highlighter(),
+            highlights = {
+                accent = wilder.make_hl('WilderAccent', 'Pmenu', {
+                    {a = 1}, {a = 1}, {foreground = '#ca62f8'}
+                })
+            },
+            left = {' ', wilder.popupmenu_devicons()},
+            right = {' ', wilder.popupmenu_scrollbar()}
+        }
+        wilder.set_option('renderer', wilder.popupmenu_renderer(opts))
+    end
+}
+
+-- Show lines for indents
+use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+        require("indent_blankline").setup {show_current_context = true}
+        vim.g.indent_blankline_filetype_exclude = {
+            'alpha', 'terminal', 'lspinfo', 'checkhealth', 'help'
+        }
+    end
+}
+
+-- Fix broken html syntax in php
+use {"captbaritone/better-indent-support-for-php-with-html", ff = "php"}
