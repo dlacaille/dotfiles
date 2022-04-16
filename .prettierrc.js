d184a44e2d1704c5e9b8b6d692432f8f099797d4
Dominic Lacaille <dominic@swoo.ca>
2022-04-15 23:16:23 -0400

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * This is the global prettier config to be used in projects without a prettier config.
+ * To use with pretierd add this to your favorite shell's variables
+ * PRETTIERD_DEFAULT_CONFIG /Users/$USER/.config/.prettierrc.js
+ */
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
