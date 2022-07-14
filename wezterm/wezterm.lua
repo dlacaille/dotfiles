4c7f7c9c7038bb3daa8f4d7e9cc36bbd0ccbf7c1
Dominic Lacaille <dominic@swoo.ca>
2022-07-14 13:34:08 -0400

    Fix <C-^> on neovim in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,22 +7,6 @@ wezterm.on("update-right-status", function(window, pane)
 	-- "Wed Mar 3 08:14"
 	local date = wezterm.strftime("%a %b %-d %H:%M ")
 
-	-- local bat = ""
-	-- for _, b in ipairs(wezterm.battery_info()) do
-	--     local baticon = wezterm.nerdfonts.mdi_battery
-	--     if b.state == "Charging" or b.state == "Full" then
-	--         baticon = wezterm.nerdfonts.mdi_battery_charging
-	--     else
-	--         if b.state_of_charge < 0.95 then
-	--             local index = math.floor(b.state_of_charge * 9 + 0.5)
-	--             baticon = wezterm.nerdfonts["mdi_battery_" + (index + 1) * 10]
-	--         end
-	--     end
-	--     bat =
-	--         baticon .. " " .. string.format("%.0f%%", b.state_of_charge * 100) ..
-	--             " " .. b.state
-	-- end
-
 	window:set_right_status(wezterm.format({
 		{ Text = wezterm.nerdfonts.fa_terminal .. " wezterm  " },
 		{ Foreground = { AnsiColor = "Red" } },
@@ -33,42 +17,15 @@ wezterm.on("update-right-status", function(window, pane)
 	}))
 end)
 
--- wezterm.on("format-tab-title",
---            function(tab, tabs, panes, config, hover, max_width)
---     local bar_background = "#0e0e14"
---     local background = "#13141c"
---     local edge_background = background
---     local edge_foreground = background
---     local foreground = "#3b4261"
---     if tab.is_active then
---         background = "#1a1b26"
---         foreground = "#c0caf5"
---         edge_background = "#7aa2f7"
---         edge_foreground = bar_background
---     elseif hover then
---         background = "#1a1b26"
---         foreground = "#c0caf5"
---         edge_background = background
---         edge_foreground = background
---     end
---     -- ensure that the titles fit in the available space,
---     -- and that we have room for the edges.
---     local title = wezterm.truncate_right(tab.active_pane.title, max_width - 3)
---     return {
---         {Background = {Color = edge_background}},
---         {Foreground = {Color = edge_foreground}},
---         {Text = LEFT_FIVE_EIGHTS_BLOCK}, {Background = {Color = background}},
---         {Text = " "}, {Foreground = {Color = foreground}}, {Text = title},
---         {Background = {Color = background}},
---         {Foreground = {Color = bar_background}}, {Text = RIGHT_HALF_BLOCK}
---     }
--- end)
-
 return {
+	debug_key_events = true,
 	color_scheme = "tokyonight-night",
-	font = wezterm.font("MonoLisa"),
+	font = wezterm.font({
+		family = "MonoLisa",
+		weight = "Regular",
+	}),
 	font_size = 12.0,
-	line_height = 1.4,
+	line_height = 1.3,
 	use_fancy_tab_bar = false,
 	tab_max_width = 48,
 	prefer_egl = true,
@@ -78,6 +35,7 @@ return {
 	window_decorations = "RESIZE",
 	-- window_background_opacity = 0.8,
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+	keys = { { key = "raw:33", mods = "CTRL", action = wezterm.action.SendString("\u{1e}") } },
 	colors = {
 		-- cursor_bg = "#d16cfe",
 		-- cursor_fg = "#000",
