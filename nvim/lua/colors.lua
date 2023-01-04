6ea5582693797429cd1ca7883f995ee0df24f913
Dominic Lacaille <dominic@lacaille.app>
2023-01-04 00:20:13 -0500

    Remove nvim config

--- nvim/lua/colors.lua
+++
@@ -1,3 +0,0 @@
-local M = require('tokyonight.colors').setup { style = 'night' }
-
-return M
--- nvim/lua/plugins/config/mind.lua
+++
@@ -1,7 +0,0 @@
-local mind = require 'mind'
-mind.setup {
-    persistence = {
-        state_path = '~/Google Drive/mind/mind.json',
-        data_dir = '~/Google Drive/mind/data',
-    },
-}
