055c99e226b5c70e254d652e7a7727513b58944e
Dominic Lacaille <dominic@lacaille.app>
2026-09-17 15:44:07 -0400

    Fix clipboard on SSH

--- astronvim/.config/nvim/lua/polish.lua
+++ astronvim/.config/nvim/lua/polish.lua
@@ -30,3 +30,15 @@ vim.o.secure = true
 
 -- Disable swap file
 vim.o.swapfile = false
+
+-- Fix clipboard on SSH
+vim.o.clipboard = "unnamedplus"
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    -- vim.highlight.on_yank()
+    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+    copy_to_unnamedplus(vim.v.event.regcontents)
+    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+    copy_to_unnamed(vim.v.event.regcontents)
+  end,
+})
